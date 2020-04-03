27c52e6207481563f49d08ed479103ce25f32bec
Jose Cuevas <josecueram@gmail.com>
2020-04-03 14:56:05 -0600

    added basic setup to test user network layer

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
+const userRouter = require('./components/user/network')
 const config = require('../config/config')
 const PORT = config.port
 
 const app = express()
 
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
