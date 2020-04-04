8172c7aa5bd99126a3bffd970a7358ebfc46d0a1
Jose Cuevas <josecueram@gmail.com>
2020-04-04 13:30:33 -0600

    created auth component with index and controller

---
+++ api/components/auth/controller.js
@@ -0,0 +1,23 @@
+const TABLE = 'auth'
+
+module.exports = (injectedStore = require('../../../store/dummy')) => {
+    const upsert = user => {
+        const authData = {
+            id: user.id
+        }
+
+        if (user.username) {
+            authData.username = user.username
+        }
+
+        if (user.password) {
+            authData.password = user.password
+        }
+
+        return injectedStore.upsert(TABLA, authData)
+    }
+
+    return {
+        upsert
+    }
+}
