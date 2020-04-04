6ab761d96d2529d0080f4c658de1d47466ca713e
Jose Cuevas <josecueram@gmail.com>
2020-04-03 21:18:58 -0600

    created dummy db component

---
+++ store/dummy.js
@@ -0,0 +1,25 @@
+const db = {}
+
+const list = table => {
+    return db[table]
+}
+
+const get = (table, id) => {
+    let collection = list(table)
+    return collection.filter(item => item.id === id)[0] || null
+}
+
+const upsert = (table, data) => {
+    db[collection].push(data)
+}
+
+const remove = (table, id) => {
+    return true
+}
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    remove
+}
