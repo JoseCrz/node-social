04673dada174d465f9c986519bf30ebfb3b13530
Jose Cuevas <josecueram@gmail.com>
2020-04-05 19:36:29 -0500

    implemented dotenv for config file

--- config/config.js
+++ config/config.js
@@ -1,4 +1,14 @@
-module.exports = {
+require('dotenv').config()
+const config = {
+    dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'notSoSecret!'
+    jwtSecret: process.env.JWT_SECRET,
+    sql: {
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        host: process.env.SQL_HOST,
+        name: process.env.SQL_NAME
+    }
+
 }
+module.exports = config
